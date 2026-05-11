40e137d1da72d43c6c3bf2841f916021e6482ff8
anuragmandal07 <anuragmandal@iisc.ac.in>
2026-05-11 15:31:56 +0530

    Add files via upload
    Add Problem Set 1

---
+++ p1.1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 02:54:28 2026
+
+@author: ANURAG MANDAL
+"""
+
+"""Getting the inputs"""
+annual_salary = float(input("Enter your annual salary:"))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
+total_cost = float(input("Enter the cost of your dream home:"))
+
+"""Intitial Conditions"""
+portion_down_payment = 0.25
+current_savings = 0
+r = 0.04
+months = 0
+down_payment = portion_down_payment*total_cost
+
+"""Calculations"""
+while current_savings < down_payment:
+    monthly_return = current_savings*r/12
+    monthly_savings = annual_salary/12*portion_saved
+    current_savings += monthly_return + monthly_savings
+    months +=1
+    
+"""Output"""
+print("Number of months: ", months)
+    
